696ed15406b65ac09a78783448c38fa8a08d500a
Yoann <yoann.letacq1@gmail.com>
2023-07-08 17:24:24 +0200

    set all the call function in the main

---
+++ sudoku-solver/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"sudoku/sudoku-solver/display"
+	"sudoku/sudoku-solver/grid"
+	"sudoku/sudoku-solver/solver"
+)
+
+func main() {
+	// get args
+	args := os.Args[1:]
+
+	// create a new grid
+	sudoku := grid.New(args)
+	if !grid.IsValid(args) {
+		return
+	}
+
+	// display the non-solve grid
+	fmt.Println("Your grid is:")
+	display.Display(sudoku)
+
+	fmt.Println()
+	// display the solved grid
+	fmt.Println("The solved grid is:")
+	// start at 0,0 top left corner of the grid
+	solved := solver.Solver(0, 0, sudoku)
+	if solved {
+		display.Display(sudoku)
+	} else {
+		fmt.Println("[ERROR] Failed to solve the grid...")
+	}
+}
