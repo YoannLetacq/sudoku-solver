fcded6016bc336b827139b443337a5bb107f01b2
Yoann <yoann.letacq1@gmail.com>
2023-07-08 11:38:55 +0200

    create the recursive solver functions

---
+++ sudoku-solver/solver/next.go
@@ -0,0 +1,12 @@
+package solver
+
+func Next(x, y int, grid [][]int) (int, int, [][]int) {
+	// set the next coordinate
+	nextX := (x + 1) % 9
+	nextY := y
+	// if we are at the end of the line change thr row
+	if nextX == 0 {
+		nextY = y + 1
+	}
+	return nextX, nextY, grid
+}
---
+++ sudoku-solver/solver/solver.go
@@ -0,0 +1,35 @@
+package solver
+
+import "sudoku/checker"
+
+// solve the given grid
+func Solver(x, y int, grid [][]int) bool {
+	// if we are at the end of the grid
+	if y == 9 {
+		return true
+	}
+
+	if grid[y][x] != 0 {
+		// skip the fill case
+		return Solver(Next(x, y, grid))
+	} else {
+		// solve the current case
+		for i := range [9]int{} {
+			v := i + 1
+			// look for doubles
+			if checker.Checker(x, y, v, grid) {
+				// no double place value
+				grid[y][x] = v
+
+				// solve the next case while it's true
+				if Solver(Next(x, y, grid)) {
+					return true
+				}
+				// if the value doesn't match set back 0
+				grid[y][x] = 0
+
+			}
+		}
+	}
+	return false
+}
